085dbbe32b69f6f1cda10f5f3f9d39417dc58d2b
tchang135 <tchang135@gmail.com>
2022-09-20 09:43:42 -0400

    First save

---
+++ guessing_game_project/lib/guessing_game.rb
@@ -0,0 +1,33 @@
+class GuessingGame
+    attr_reader :num_attempts, 
+    def initialize(min, max)
+        p "Please enter a min number"
+        @min = gets.chomp.to_i
+
+        p "Please enter a max number"
+        @max = gets.chomp.to_i
+
+        @secret_num = rand(min..max)
+        @num_attempts = 0
+        @game_over = false
+    end 
+
+    def game_over?
+        @game_over
+    end 
+
+    def check_num(num)
+        @num_attempts += 1
+        if @secret_num == num
+            @game_over = true 
+            p "you win"
+        elsif @secret_num > num 
+            p "too small"
+        else  
+            p "too big"
+        end 
+    end 
+    
+
+
+end
