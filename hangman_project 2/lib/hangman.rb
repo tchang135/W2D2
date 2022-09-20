cbb859aab679e8ec1f2d585a1ce19b6bebeaf903
tchang135 <tchang135@gmail.com>
2022-09-20 11:05:36 -0400

    Finished part 1 hangman

--- hangman_project 2/lib/hangman.rb
+++ hangman_project 2/lib/hangman.rb
@@ -1,4 +1,5 @@
 class Hangman
+  attr_reader :guess_word, :attempted_chars, :remaining_incorrect_guesses
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
 
   def self.random_word
@@ -12,5 +13,29 @@ class Hangman
     @guess_word = Array.new(@secret_word.length, '_')            
   end 
 
+  def already_attempted?(char)
+    if @attempted_chars.include?(char)
+      return true 
+    else  
+      return false 
+    end 
+  end 
+
+  def get_matching_indices(char)
+  indices = [] 
+
+    @secret_word.each_char.with_index do |ele, i|
+      if ele == char 
+        indices << i
+      end 
+    end 
+  indices
+  end 
+
+  def fill_indices(char, arr)
+    arr.each do |num|
+      @guess_word[num] = char
+    end 
+  end 
 
 end
