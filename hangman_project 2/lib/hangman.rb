9b0ea27689ebe7f9482a6567a41715b81136244d
tchang135 <tchang135@gmail.com>
2022-09-20 10:21:59 -0400

    Finished Initialize on hangman

--- hangman_project 2/lib/hangman.rb
+++ hangman_project 2/lib/hangman.rb
@@ -1,4 +1,16 @@
 class Hangman
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
 
+  def self.random_word
+    DICTIONARY.sample
+  end 
+
+  def initialize
+    @secret_word = Hangman.random_word
+    @attempted_chars = []
+    @remaining_incorrect_guesses = 5      
+    @guess_word = Array.new(@secret_word.length, '_')            
+  end 
+
+
 end
