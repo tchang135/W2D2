c08e02a3f4cb9640606f67a203ae32e8c366e624
tchang135 <tchang135@gmail.com>
2022-09-20 13:50:19 -0400

    Finished hangman

--- hangman_project 2/lib/hangman.rb
+++ hangman_project 2/lib/hangman.rb
@@ -37,5 +37,55 @@ class Hangman
       @guess_word[num] = char
     end 
   end 
+  #PART 2
+  def try_guess(char)
+    if self.already_attempted?(char)
+      p "that has already been attempted"
+      return false 
+    else  
+      @attempted_chars << char 
+    end 
+     
+    if !@secret_word.include?(char)
+        @remaining_incorrect_guesses -= 1 
+    else  
+        idx = self.get_matching_indices(char)
+        self.fill_indices(char, idx)
+    end 
+  true 
+  end 
+
+  def ask_user_for_guess 
+    p 'Enter a char:'
+    response = gets.chomp 
+    self.try_guess(response)
+  end 
+
+  def win?
+    if @guess_word.join("") == @secret_word
+      p 'WIN'
+      return true 
+    else  
+      return false 
+    end 
+  end 
+
+  def lose?
+    if @remaining_incorrect_guesses == 0 
+      p 'LOSE'
+      return true 
+    else  
+      return false 
+    end 
+  end 
+
+  def game_over?
+    if self.win? || self.lose?
+      print @secret_word
+      return true 
+    else  
+      return false 
+    end 
+  end 
 
 end
