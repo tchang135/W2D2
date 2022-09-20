5accf9a3ac8ca3dc5f94f1fe556d07624aeffad1
tchang135 <tchang135@gmail.com>
2022-09-20 15:19:08 -0400

    Halfway part 2 hotel

--- hotel_project/lib/hotel.rb
+++ hotel_project/lib/hotel.rb
@@ -1,6 +1,7 @@
 require_relative "room"
 
 class Hotel
+    attr_reader :rooms
     def initialize(name, capacity_of_room)
         @name = name 
         @rooms = {}
@@ -9,4 +10,26 @@ class Hotel
             @rooms[room_name] = Room.new(capacity)
         end 
     end 
+
+    def name 
+        words = @name.split(" ")
+        hotel_name = words.map do |word|
+            word.capitalize
+        end 
+    hotel_name.join(" ")
+    end 
+
+    def room_exists?(name)
+        if @rooms.has_key?(name)
+            return true 
+        else  
+            return false 
+        end 
+    end 
+
+    def check_in
+
+    end 
+
+
 end
